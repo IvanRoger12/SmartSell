5cac1c55dd6023077ca31f1e930f37938d6cadfa
Ivan NFINDA <151882341+IvanRoger12@users.noreply.github.com>
2025-07-28 12:57:16 +0200

    Delete app_smartsell_final_supercharged.py

--- app_smartsell_final_supercharged.py
+++
@@ -1,7 +0,0 @@
-# Ce fichier contiendra toutes les améliorations demandées :
-# - Graphe radar ou sunburst
-# - Insight IA automatique
-# - Message LinkedIn ready
-# - Mode sombre ou vue "détaillée/minimaliste"
-# - Icône animée à l'export
-# - Zone de feedback utilisateur
